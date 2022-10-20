41a517ff6dbc2c87b3c8f0f8889f74ba5349054d
bryan <bryan@BRYAN>
2022-10-19 19:11:48 -0500

    Agregar archivos de proyecto.

---
+++ semanaDos1/Regristro.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semanaDos1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Regristro : ContentPage
+    {
+        public Regristro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInfo_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            double nota1 = Convert.ToDouble(txtNotaUno.Text);
+            double nota2 = Convert.ToDouble(txtNotaDos.Text);
+            double nota =(nota1+nota2)/2;
+
+               Resultado.Text=nota.ToString();
+
+
+            if (nota>=7)
+            {
+                DisplayAlert("Mensaje de alerta", "Felicidades" + nombre + "\n " + apellido + "\n Aprobado: "+nota, "Cerrar");
+            }
+            else
+            {
+                DisplayAlert("Mensaje de alerta", "Estimado:" + nombre + "" + apellido + "Reprobado con "+nota, "Cerrar");
+
+            }
+            
+           
+        }
+    }
+}
---
+++ semanaDos1/login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semanaDos1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class loguin : ContentPage
+    {
+        public loguin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicio_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Regristro());
+
+            string usuario = "Bryan";
+            string contrasena = "123456";
+            string tusuario = txtUser.Text;
+            string tcontrasena = txtContraseña.Text;
+
+            if (usuario == tusuario & contrasena == tcontrasena) 
+            {
+                DisplayAlert("Alerta", "Usuario correcto", "Cerrar");
+                //Navigation.PushAsync(new Regristro());
+                
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario no existe", "Cerrar");
+
+            }
+
+
+        }
+    }
+}
